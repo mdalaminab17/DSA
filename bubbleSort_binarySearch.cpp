da1fa29041cf17f06a7860b09a98a73f5840f5c1
mdalaminab17 <mdalaminab17@gmail.com>
2025-08-11 16:47:55 +0600

    Update bubbleSort_binarySearch.cpp

--- bubbleSort_binarySearch.cpp
+++ bubbleSort_binarySearch.cpp
@@ -1,1 +1,61 @@
+#include <iostream>
+using namespace std;
+    void bubblesort(int nums[], int n){
+        for(int i=0; i<n-1; i++){
+           bool f = 0;
+           for(int j=0; j<n-1-i; j++){
+              if(nums[j]>nums[j+1]){
+                 int temp = nums[j];
+                 nums[j] = nums[j+1];
+                 nums[j+1] = temp; //swap(nums[j],nums[j+1]);
+                 f = 1;
+              } 
+           } if(!f){
+                break;
+           }
+        }
+    }
 
+    int binarySearch(int nums[], int n, int target){
+        int st = 0;
+        int end = n-1;
+        
+        while(st<=end){
+            int mid = st + (end - st)/2;
+            if(nums[mid] == target){
+                return mid;
+            }else if(nums[mid] > target){
+                end = mid-1;
+            }else{
+                st = mid + 1;
+            }
+        } return -1;
+    }
+
+
+int main(){
+    int n;
+    cout<<"Enter Array Size: ";
+    cin>>n;
+    int nums[n];
+    int target;
+    
+    cout<<"Enter Array Elements: ";
+    for(int i=0;i<n;i++){
+        cin>>nums[i];
+    }
+    cout<<"Enter Target: ";
+    cin>>target;
+    
+    bubblesort(nums,n);
+    
+    int found = binarySearch(nums,n,target);
+    if(found == -1){
+        cout<<"Target Not Found";
+    }else{
+        cout<<"Target Found at index " <<found;
+    }
+    
+    cout << endl;
+
+}
