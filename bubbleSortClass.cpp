c311dfbab4ac81d0dc1860ec8f87ba7e4d2f7b8c
mdalaminab17 <mdalaminab17@gmail.com>
2025-08-11 16:13:44 +0600

    bubbleSortClass.cpp
    Bubble Sort Using Member Function of a Class

---
+++ bubbleSortClass.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+class Solution{
+    public:
+    void bubblesort(int nums[], int n){
+        for(int i=0; i<n-1; i++){
+           bool f = 0;
+           for(int j=0; j<n-1-i; j++){
+              if(nums[j]>nums[j+1]){
+                 int temp = nums[j];
+                 nums[j] = nums[j+1];
+                 nums[j+1] = temp; //swap(nums[j],nums[j+1]);
+                 f = 1;
+              } 
+           } if(!f){
+                break;
+           }
+        }
+    }
+
+};
+
+
+int main(){
+    int n;
+    cout<<"Enter Array Size: ";
+    cin>>n;
+    int nums[n];
+    
+    cout<<"Enter Array Elements: ";
+    for(int i=0;i<n;i++){
+        cin>>nums[i];
+    }
+    
+    Solution s1;
+    
+    s1.bubblesort(nums,n);
+    cout <<"Sorted Array: ";
+    for (int i = 0; i < n; i++) {
+        cout << nums[i] << " ";
+    }
+    cout << endl;
+
+}
